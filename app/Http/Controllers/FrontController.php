a8000dddca600f2a66f6aa494ba4f4fdea6fef0b
Guruhg19 <guruhvitra19@gmail.com>
2025-04-09 11:45:28 +0700

    Add brand view and update routing for product details

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
+use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -18,4 +20,15 @@ class FrontController extends Controller
         session()->put('category_id', $category->id);
         return view('front.brands',compact('category'));
     }
+
+    public function brand(Brand $brand){
+        $category_id = session()->get('category_id');
+        $products = Product::where('brand_id',$brand->id)
+        ->where('category_id', $category_id)
+        ->latest()
+        ->get();
+        
+        return view('front.gadgets',compact('brand','products'));
+    }
+
 }

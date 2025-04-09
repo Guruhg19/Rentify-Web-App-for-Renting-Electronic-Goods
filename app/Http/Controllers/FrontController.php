de27c7be702413a32e3ad2e21052bde3313b4c95
Guruhg19 <guruhvitra19@gmail.com>
2025-04-09 11:15:07 +0700

    Add category view for brand selection with responsive layout

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
@@ -12,5 +12,10 @@ class FrontController extends Controller
         $latest_products = \App\Models\Product::latest()->take(4)->get();
         $random_products = \App\Models\Product::inRandomOrder()->take(4)->get();
         return view('front.index',compact('categories', 'latest_products', 'random_products'));
+    }
+
+    public function category(Category $category){
+        session()->put('category_id', $category->id);
+        return view('front.brands',compact('category'));
     }
 }
